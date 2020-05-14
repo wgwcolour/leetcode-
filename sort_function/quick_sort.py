42dac675bebfc3b5b4b829f946f96325f319ea53
wangguangwei <wgwxuexi@gmail.com>
2020-05-14 18:52:43 +0800

    add exercise

--- sort_function/quick_sort.py
+++ sort_function/quick_sort.py
@@ -31,7 +31,34 @@ def partition(li, l, r):
     return i
 
 
+def exercise(li, k):
+    """
+    练习题：找到数组li中的第k大的元素
+    例如li=[3,6,1,2,8],k=3,返回值为3
+    :param li:
+    :param k:
+    :return:
+    """
+
+    def get_k(li, l, r, k):
+        mid = partition(li, l, r)
+        # [1,2,3,4,5,6]
+        if k == len(li) - mid: # mid的位置正好是第k大
+            return li[mid]
+        elif len(li) - k > mid: # 第mid大的元素大于第k大，k在右边的数组中
+            return get_k(li, mid + 1, r, k)
+        else: # k在左边的数组中
+            return get_k(li, l, mid - 1, k)
+
+    n = len(li)
+    l = 0
+    r = n - 1
+    i = get_k(li, l, r, k)
+    print(i)
+
+
 if __name__ == '__main__':
     li = [6, 82, 89, 23, 2, 98, 0, 2, 56, 8, 2, 1, 6, 9, 54345, 923, 0, 23, 7, 456, 57, 908, 232]
     quick_sort_main(li)
     print(li)
+    exercise(li, 2)
